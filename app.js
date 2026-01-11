174f3f2bd1eda6f3b20427fb944418daa0c0a7df
Tanja Mueller <tmueller19@gmx.de>
2026-01-11 21:21:54 +0100

    Solved Lab

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
-
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model")
 const app = express();
 
+  
 // MIDDLEWARE
 app.use(logger("dev"));
 app.use(express.static("public"));
@@ -11,7 +13,12 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URL = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
+mongoose
+  .connect(MONGODB_URL)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo:", err));
 
 
 // ROUTES
@@ -23,23 +30,90 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
+app.post("/recipes", (req, res) => {
 
+  Recipe.create({
+    title: req.body.title,
+    instructions: req.body.instructions,
+    level: req.body.level,
+    ingredients: req.body.ingredients,
+    image: req.body.image,
+    duration: req.body.duration,
+    isArchived: req.body.isArchived,
+    created: req.body.created
+  })
+
+    .then((createdRecipe) => {
+      console.log("Recipe created")
+      res.status(201).json(createdRecipe);
+    })
+    .catch((err) => {
+      console.error("Internal Server Error while creating recipe:", err)
+      res.status(500).json({ message: "Internal Server Error while trying to create recipe", err})
+    })
+});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get("/recipes", (req, res) => {
+  Recipe.find()
+  .then(recipes => {
+    console.log("All recipes found")
+    res.status(200).json(recipes)
+  })
+  .catch((err) => {
+    console.error("Internal Server Error while trying to get all recipes:", err)
+    res.status(500).json({ message: "Internal Server Error while trying to get all recipes", err})
+  })
+  })
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+app.get("/recipes/:id", (req, res) => {
+  const { id } = req.params
 
+  Recipe.findById(id)
+  .then(recipe => {
+    console.log("Recipe found")
+    res.status(200).json(recipe);
+  })
+  .catch((err) => {
+    console.error("Internal Server Error while trying to get recipe:", err)
+    res.status(500).json({ message: "Internal Server Error while trying to get recipe", err})
+  })
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
+app.put("/recipes/:id", (req, res) => {
+const { id } = req.params
 
+  Recipe.findByIdAndUpdate(id, req.body, { new: true })
+    .then((updatedRecipe) => {
+      console.log("Recipe updated")
+      res.status(200).json(updatedRecipe);
+    })
+    .catch((err) => {
+      console.error("Internal Server Error while trying to update recipe:", err)
+      res.status(500).json({ message: "Internal Server Error while trying to update recipe", err})
+    })
+  })
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
+app.delete("/recipes/:id", (req, res) => {
+  const { id } = req.params
 
+  Recipe.findByIdAndDelete(id)
+  .then((deletedRecipe) => {
+    console.log("Recipe deleted")
+    res.status(204).json({ message: "Recipe was successfully deleted"})
+  })
+  .catch((err) => {
+    console.error("Internal Server Error while trying to delete recipe:", err)
+    res.status(500).json({ message: "Internal Server Error while trying to delete recipe", err})
+  })
+})
 
 
 // Start the server
