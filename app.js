02d5af9fdc2058c040812ca7f62d8e5731174a40
Tanja Mueller <tmueller19@gmx.de>
2026-01-11 21:31:46 +0100

    Solved Lab

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
-const MONGODB_URL = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
 mongoose
-  .connect(MONGODB_URL)
+  .connect(MONGODB_URI)
   .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch((err) => console.error("Error connecting to mongo:", err));
 
